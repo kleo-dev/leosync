6a409de03c592cc62baa65ceb2c8cf156d481489
Leo dev <klesti.selimay@icloud.com>
2024-11-17 19:10:09 +0100

    oops

---
+++ leosync.py
@@ -0,0 +1,62 @@
+import os
+from os import system as cmd
+import subprocess
+import re
+from sys import argv
+
+class config:
+    REPO = '' # your Git repository url
+    PATH = '' # your projects path (AKA where your github repo is located), example: /home/leo/projects/
+
+def run(cmd):
+    return subprocess.run(cmd, capture_output=True, text=True)
+
+INFO = '\x1b[34m[INFO]\x1b[0m'
+ERROR = '\x1b[31m[ERROR]\x1b[0m'
+WARNING = '\x1b[33m[WARNING]\x1b[0m'
+
+if len(argv) != 2:
+    print(f'{ERROR} Invalid command usage: leosync <pull | push>')
+    exit(-1)
+
+git_output = run(['git', '-v'])
+
+if git_output.returncode != 0:
+    print(f'{ERROR} Please install git: \x1b[4mhttps://git-scm.com\x1b[0m')
+    exit(-1)
+
+git_version = re.findall(r'\d.\d+.\d', git_output.stdout)[0]
+
+print(f'{INFO} Using Git version \x1b[32m{git_version}\x1b[0m')
+
+def pull():
+    if os.path.exists(config.PATH):
+        print(f'{INFO} Pulling')
+        prev = os.getcwd()
+        os.chdir(config.PATH)
+        run(['git', 'pull'])
+        os.chdir(prev)
+    else:
+        print(f'{INFO} Cloning')
+        run(['git', 'clone', config.REPO])
+
+def push():
+    print(f'{INFO} Pushing')
+    prev = os.getcwd()
+    os.chdir(config.PATH)
+    cmd('git add -A')
+    cmd('git commit -am "Sync"')
+    cmd('git push')
+    os.chdir(prev)
+
+if argv[1] == 'pull':
+    pull()
+elif argv[1] == 'push':
+    push()
+elif argv[1] == 'sync':
+    print(f'{INFO} Syncing')
+    pull()
+    push()
+else:
+    print(f'{ERROR} Invalid command usage: leosync <pull | push>')
+    exit(-1)
